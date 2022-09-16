56069adc35b6ffc7f5d32788903fd2a59484e091
unknown <freelanceryousuf89@gmail.com>
2022-09-17 01:38:20 +0600

    complit

---
+++ js/utilities.js
@@ -0,0 +1,19 @@
+function getInputFieldValueById(inputFieldId) {
+  const inputField = document.getElementById(inputFieldId);
+  const inputFieldValueString = inputField.value;
+  const inputFieldValue = parseFloat(inputFieldValueString);
+  inputField.value = "";
+  return inputFieldValue; // return korer por kono fuc.. call hobe na
+}
+
+function getTextElementValueById(elementId) {
+  const textElement = document.getElementById(elementId);
+  const textElementValueString = textElement.innerText;
+  const textElementValue = parseFloat(textElementValueString); //parseFloat mane value ta ke convart koror jonno kora hoyea theka
+  return textElementValue;
+}
+
+function setTextElementValueById(elementId, newValue) {
+  const textElement = document.getElementById(elementId);
+  textElement.innerText = newValue;
+}
